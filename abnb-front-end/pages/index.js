dd45a6456c90fc0a1d11c696dc8fefc4ce6aa8fb
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-04-29 19:26:59 -0500

    ⚡1

--- abnb-front-end/pages/index.js
+++ abnb-front-end/pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
-  return (
-    <div>
-      <h3>Hello</h3>
-    </div>
-  );
-}
+import { sanityClient } from "../sanity";
+
+const Home = ({ properties }) => {
+  console.log(properties);
+  return <></>;
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[ _type == "property"]';
+  const properties = await sanityClient.fetch(query);
+  console.log(properties);
+
+  if (!properties.length) {
+    return {
+      props: {
+        properties: [],
+      },
+    };
+  } else {
+    return {
+      props: {
+        properties,
+      },
+    };
+  }
+};
+
+export default Home;
